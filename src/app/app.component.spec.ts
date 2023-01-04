5ef639d6e2511c45829e31deb237ca76334fd33c
Francesco Bossoli <francescobossoli@hotmail.com>
2023-01-04 16:48:22 +0100

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BuildWeek3'`, () => {
+  it(`should have as title 'build-week3-fe0622a'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BuildWeek3');
+    expect(app.title).toEqual('build-week3-fe0622a');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BuildWeek3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('build-week3-fe0622a app is running!');
   });
 });
